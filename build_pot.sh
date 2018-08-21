7d2c576c218813145b9565620ce4fb69d495de00
tronfortytwo <emanuele.sorce@hotmail.com>
2018-08-21 08:03:20 +0200

    fixes

--- build_pot.sh
+++ build_pot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xgettext -o po/com.ubuntu.developer.andrew-hayzen.volleyball2d.pot --package-name volleyball2d --qt --c++ --add-comments=TRANSLATORS --keyword=tr `find . -type f -name "*.qml"`
+xgettext -o po/volleyball2d.emanuelesorce.pot --package-name volleyball2d --qt --c++ --add-comments=TRANSLATORS --keyword=tr `find . -type f -name "*.qml"`
